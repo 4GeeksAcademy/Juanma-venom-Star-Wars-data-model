efa5b52cf85eca4e45291056fa2ee4bd55fb23f1
PRODY (Juanma) <69425794+Juanma-venom@users.noreply.github.com>
2025-07-14 17:54:07 +0000

    Star-Wars Data

--- src/models.py
+++ src/models.py
@@ -26,7 +26,7 @@ class Usuario(db.Model):
     username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
 
-    guardar = relationship("Guardar", back_populates="usuario")
+    favoritos = relationship("Favoritos", back_populates="usuario")
 
     def serialize(self):
         return {
@@ -43,8 +43,7 @@ class Planetas(db.Model):
     planeta: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     n_guardados: Mapped[str] = mapped_column(nullable=False)
 
-    id_guardar = mapped_column(ForeignKey("guardar.id"))
-    guardar = relationship("Guardar", back_populates="planetas")
+    favoritos = relationship("Favoritos", back_populates="planetas")
 
     def serialize(self):
         return {
@@ -60,9 +59,9 @@ class Planetas(db.Model):
 class Personajes(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     personajes: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    favoritos_id = mapped_column(ForeignKey("favoritos.id"))
 
-    id_guardar = mapped_column(ForeignKey("guardar.id"))
-    guardar = relationship("Guardar", back_populates="personajes")
+    favoritos = relationship("Favoritos", back_populates="personajes")
 
     def serialize(self):
         return {
@@ -74,17 +73,23 @@ class Personajes(db.Model):
 
 
 
-class Guardar(db.Model):
+class Favoritos(db.Model):
+    __tablename__ = 'favoritos'
     id: Mapped[int] = mapped_column(primary_key=True)
-    id_personajes: Mapped[int] = mapped_column(primary_key=True)
+    id_usuario: Mapped[int] = mapped_column(primary_key=True)
     id_planetas: Mapped[int] = mapped_column(primary_key=True)
- 
-    id_usuario = mapped_column(ForeignKey("usuario.id"))
-    personajes = relationship("Personajes", back_populates="guardar")
-    planetas = relationship("Planetas", back_populates="guardar")
+    id_personajes: Mapped[int] = mapped_column(primary_key=True)
+
+    usuario = relationship("Usuario", back_populates="favoritos")
+    personajes = relationship("Personajes", back_populates="favorito")
+    planetas = relationship("Planetas", back_populates="favorito")
 
     def serialize(self):
         return {
             "id": self.id,
+            "usuario_id": self.usuario_id,
+            "planeta": self.planeta.serialize() if self.planeta else None,
+            "personaje": self.personaje.serialize() if self.personaje else None
             # do not serialize the password, its a security breach
         }
+
